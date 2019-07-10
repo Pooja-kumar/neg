f0ff0b5e3d35e372f2103c072b914d445eb93292
pooja sree <46490818+Pooja-kumar@users.noreply.github.com>
2019-07-10 20:17:53 +0530

    Update vow.py

--- vow.py
+++ vow.py
@@ -1,7 +1,10 @@
-c = input()
-
-if(c=='A' or c=='a' or c=='E' or c=='e' or c=='I'
- or c=='i' or c=='O' or c=='o' or c=='U' or c=='u'):
-    print("Vowel")
+c=input('')
+if(c.isalpha()):
+	if(c=='a'or c=='A'or c=='e'or c=='E'or c=='i'or c=='I'or c=='o'or c=='O'or c=='u'or c=='U'):
+		print("Vowel") 
+	elif(c!='a'or c!='A'or c!='e'or c!='E'or c!='i'or c!='I'or c!='o'or c!='O'or c!='u'or c!='U'):
+		
+		print("Consonant")
+	
 else:
-    print("Consonant")
+	print("Invalid")
