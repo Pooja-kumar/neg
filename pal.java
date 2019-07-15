0c4278df509a7e9198c8dd71be067ea28f05b565
pooja sree <46490818+Pooja-kumar@users.noreply.github.com>
2019-07-15 11:20:45 +0530

    Create pal.java

---
+++ pal.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner obj= new Scanner(System.in);
+	    String d=obj.nextLine();
+	    StringBuilder sb = new StringBuilder(d);
+	    String b=sb.reverse().toString();
+	    System.out.println(b);
+	     System.out.println(d);
+	    if(d.equals(b)){
+	    	System.out.println("yes");
+	   }
+	    else{
+	    	System.out.println("no");
+	    }
+	    
+	}
+}
